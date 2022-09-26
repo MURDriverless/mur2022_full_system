543033e04da2538a1f1de1a2455de73351382762
Micah-Swenson <m.swenson@optusnet.com.au>
2022-09-26 16:54:14 +1000

    Added Control Translation node

--- catkin_ws/src/mur2022/src/output_transformation_node.cpp
+++ catkin_ws/src/mur2022/src/output_transformation_node.cpp
@@ -1,13 +1,17 @@
 #include "ros/ros.h"
 #include <ros/package.h>
 #include <std_msgs/Bool.h>
+#include <geometry_msgs/Twist.h>
 #include "topic_names.h"
 
 // Declare "member" variables
 ros::Subscriber control_output_sub;
+ros::Publisher control_output_pub;
 
 // Respond to subscriber receiving a message
-void sendControlOutputs(const std_msgs::Bool& msg){}
+void sendControlOutputs(const geometry_msgs::Twist msg){
+	control_output_pub.publish(msg);
+}
 
 int main(int argc, char* argv[]) {
 	// Initialise the node
@@ -15,7 +19,8 @@ int main(int argc, char* argv[]) {
 	ros::NodeHandle nh("~");
 
 	// Initialise a subscriber
-	control_output_sub = nh.subscribe(CONTROL_OUTPUT_TOPIC, 1, sendControlOutputs);
+	control_output_sub = nh.subscribe("/husky_velocity_controller/cmd_vel", 1, sendControlOutputs);
+	control_output_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 
 	// Spin as a single-threaded node
 	ros::spin();
