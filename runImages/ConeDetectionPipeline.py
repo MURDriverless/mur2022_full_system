6dd3ffab1b95545f988d13a8dcdda80b604d4c68
Micah Swenson <m.swenson@optusnet.com.au>
2022-10-06 21:52:16 +1100

    Moved the images take 2

--- runImages/ConeDetectionPipeline.py
+++ runImages/ConeDetectionPipeline.py
@@ -1,20 +1,26 @@
 
 from cmath import inf
+from random import randint, random
 import cv2 as cv
 import numpy as np
+from numpy import infty
 
 # Camera Parameters (Left is camera 1)
-mtxL = np.array([[1.43643943e+03, 0.00000000e+00, 9.20883584e+02], [0.00000000e+00, 1.44182535e+03, 5.90056085e+02],[0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
-dstL = np.array([[-1.76601573e-01,  2.47679371e-01, -1.55554300e-04,  3.37828665e-03,  -1.29219184e-01]])
-mtxR = np.array([[2.13852629e+03, 0.00000000e+00, 9.42381208e+02],[0.00000000e+00, 2.13179489e+03, 5.87702763e+02],[0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
-dstR = np.array([[-0.21975105, -0.18125776,  0.00486067, -0.00421931,  2.51366215]])
+mtxL = np.array([[1.38091983e+03, 0.00000000e+00, 9.29095040e+02],
+ [0.00000000e+00, 1.38408640e+03, 5.93175950e+02],
+ [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+dstL = np.array([[-0.15539337,  0.1543956,   0.00031466,  0.00135602, -0.02925023]])
+mtxR = np.array([[1.40910150e+03, 0.00000000e+00, 9.36842563e+02],
+ [0.00000000e+00, 1.41196863e+03, 5.86126604e+02],
+ [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+dstR = np.array([[-0.16959046,  0.21172638, -0.00265575, -0.00280348, -0.09712112]])
 
-E = np.array([[ 0.00048316, -0.45258333, -0.02748465],
- [ 0.45243798, -0.01139074,  0.30094337],
- [ 0.00996433, -0.29959732, -0.01176402]])
-F = np.array([[ 5.10707252e-09, -4.76602956e-06,  2.39020957e-03],
- [ 4.79746417e-06, -1.20331506e-07,  2.36887107e-04],
- [-2.59905548e-03, -2.18486384e-03,  1.00000000e+00]])
+E = np.array([[ 2.03103864e-04, -2.05738398e-02,  1.12819991e-03],
+ [ 3.82500399e-02, -6.76106484e-03,  2.27036723e-01],
+ [-1.00321863e-03, -2.29321873e-01, -6.81895235e-03]])
+F = np.array([[ 1.22950886e-07, -1.24260786e-05,  8.19974018e-03],
+ [ 2.31080127e-05, -4.07522012e-06,  1.70354458e-01],
+ [-1.45151654e-02, -1.81137177e-01,  1.00000000e+00]])
 
 # Projection Matrices
 PL = np.array([[1.43643943e+03, 0.00000000e+00, 9.20883584e+02, 0.00000000e+00],
@@ -26,7 +32,7 @@ PR = np.array([[ 2.13576837e+03, -3.51667421e+01,  9.47962971e+02, -2.14016578e+
  [-2.91575222e-03, -3.93325159e-02,  9.99221923e-01,  4.53314386e-01]])
 
 # Rectified Parameters
-img_id = 3
+img_id = 2
 right_image = cv.imread("right/"  + str(img_id) + ".png")
 left_image = cv.imread("left/" + str(img_id) + ".png")
 right_image_og = right_image
@@ -43,8 +49,8 @@ cv.waitKey(1000)
 cv.imshow("left", left_image)
 cv.waitKey(1000)
 
-confThreshold = 0.8  #Confidence threshold
-nmsThreshold = 0.8  #Non-maximum suppression threshold
+confThreshold = 0.75  #Confidence threshold
+nmsThreshold = 0.5   #Non-maximum suppression threshold
 inpWidth = 832       #Width of network's input image
 inpHeight = 832      #Height of network's input image
 
@@ -66,7 +72,9 @@ def getOutputsNames(net):
     layersNames = net.getLayerNames()
     # Get the names of the output layers, i.e. the layers with unconnected outputs
     check = net.getUnconnectedOutLayers().tolist()
-    return [layersNames[i[0] - 1] for i in check]
+    # If error switch line
+    return [layersNames[i - 1] for i in check]
+    # return [layersNames[i[0] - 1] for i in check]
 
 # Draw the predicted bounding box
 def drawPred(image, classId, conf, left, top, right, bottom):
@@ -119,7 +127,8 @@ def postprocess(image, outs):
     oBoxes = []
     oClasses = []
     for i in indices:
-        i = i[0]
+        # If error uncomment below
+        # i = i[0]
         box = boxes[i]
         oBoxes.append(box)
         oClasses.append(classIds[i])
@@ -167,34 +176,36 @@ cv.putText(left_image, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255)
 # TODO: Find Matches between found cones
 # PROBLEM: Fundamental matrix is not great - giving bad matches = poor stereo calibration
 centerL = []
-for i in range(len(boxesL)):
-    box = boxesL[i]
+for r in range(len(boxesL)):
+    box = boxesL[r]
     x = box[0] + box[2]/2
     y = box[1] + box[3]/2
     centerL.append([x, y])
 centerL = np.array(centerL)
+print(centerL)
 
 centerR = []
-for i in range(len(boxesR)):
-    box = boxesR[i]
+for r in range(len(boxesR)):
+    box = boxesR[r]
     x = box[0] + box[2]/2
     y = box[1] + box[3]/2
     centerR.append([x, y])
 centerR = np.array(centerR)
+print(centerR)
 # centerL = cv.undistortImagePoints(np.array(centerL), mtxL, dstL)
 linesR = cv.computeCorrespondEpilines(centerL.reshape(-1,1,2), 1, F)
 linesL = cv.computeCorrespondEpilines(centerR.reshape(-1,1,2), 2, F)
-for point, cl in zip(centerL, classL):
-    x, y = point
-    if cl == 0:
-        color = (255, 0, 0)
-    elif cl == 1:
-        color = (0, 255, 0)
-    elif cl == 2:
-        color = (0, 255, 255)
-    else:
-        color = (0, 0, 0)
-    left_image = cv.circle(left_image,(int(x),int(y)),5,color,-1)
+# for point, cl in zip(centerL, classL):
+#     x, y = point
+#     if cl == 0:
+#         color = (255, 0, 0)
+#     elif cl == 1:
+#         color = (0, 255, 0)
+#     elif cl == 2:
+#         color = (0, 255, 255)
+#     else:
+#         color = (0, 0, 0)
+#     left_image = cv.circle(left_image,(int(x),int(y)),5,color,-1)
 
 for (r, cl) in zip(linesL, classR):
     if cl == 0:
@@ -212,18 +223,18 @@ for (r, cl) in zip(linesL, classR):
 
 cv.imshow("Found Cones left", left_image)
 cv.waitKey(1000)
-# 
-for point, cl in zip(centerR, classR):
-    x, y = point
-    if cl == 0:
-        color = (255, 0, 0)
-    elif cl == 1:
-        color = (0, 255, 0)
-    elif cl == 2:
-        color = (0, 255, 255)
-    else:
-        color = (0, 0, 0)
-    right_image = cv.circle(right_image,(int(x),int(y)),5,color,-1)
+#  
+# for point, cl in zip(centerR, classR):
+#     x, y = point
+#     if cl == 0:
+#         color = (255, 0, 0)
+#     elif cl == 1:
+#         color = (0, 255, 0)
+#     elif cl == 2:
+#         color = (0, 255, 255)
+#     else:
+#         color = (0, 0, 0)
+#     right_image = cv.circle(right_image,(int(x),int(y)),5,color,-1)
 
 for (r, cl) in zip(linesR, classL):
     if cl == 0:
@@ -241,18 +252,78 @@ for (r, cl) in zip(linesR, classL):
 
 cv.imshow("Found Cones right", right_image)
 cv.waitKey(1000)
-
-for i in range(len(centerR)):
-    min_dist = inf
-    min_id = None
-    for j in range(len(linesR)):
-        if classR[i] == classL[j]:
-            point = np.array([centerR[i][0], centerR[i][1], 1])
-            line = linesR[j][0]
+a = 0.5
+# Collect all distances
+all_cost = np.ones((len(centerR), len(linesR)))*inf
+for r in range(len(centerR)):
+    for l in range(len(linesR)):
+        if classR[r] == classL[l]:
+            point = np.array([centerR[r][0], centerR[r][1], 1])
+            line = linesR[l][0]
             line = np.array([line[0], line[1], line[2]])
-            dist = abs(np.dot(point, line))
+            boxL = boxesL[l]
+            boxR = boxesR[r]
+            cost = (1-a)*(abs(boxL[2] - boxR[2]) + abs(boxL[3] - boxL[3])) + a*abs(np.dot(point, line))
+            all_cost[r][l] = cost
+
+print(all_cost)
+# Pair up cones from left and right image based on distance
+pairs = []
+for r in range(len(centerR)):
+    for l in range(len(linesR)):
+        if min(all_cost[r, :]) == all_cost[r, l] and min(all_cost[:, l]) == all_cost[r, l]:
+            pairs.append((r, l))
+
+print(pairs)
+
+# Select only the right points
+sortL = []
+sortR = []
+for r, l in pairs:
+    sortR.append(r)
+    sortL.append(l)
 
 
+centerL = centerL[sortL]
+classL = np.array(classL)
+classL = classL[sortL]
+centerR = centerR[sortR]
+classR = np.array(classR)
+classR = classR[sortR]
+for pointL, pointR, cl in zip(centerL, centerR, classR):
+    xL, yL = pointL
+    xR, yR = pointR
+    if cl == 0:
+        color = (100 + randint(0, 150), 0, 0)
+    elif cl == 1:
+        color = (0, 50 + randint(0, 200), 0)
+    elif cl == 2:
+        rn = 50 + randint(0, 200)
+        color = (0, rn, rn)
+    else:
+        color = (0, 0, 0)
+    right_image = cv.circle(right_image,(int(xR),int(yR)),5,color,-1)
+    left_image = cv.circle(left_image,(int(xL),int(yL)),5,color,-1)
 
+cv.imshow("Left Image w/ matches", left_image)
+cv.waitKey(2000)
+cv.imshow("Right Image w/ matches", right_image)
+cv.waitKey(2000)
+    
 # TODO: Once Matches have been found compute cone locations
 # triangulate points
+points4D = cv.triangulatePoints(PL, PR, centerL.transpose(), centerR.transpose())
+
+points3D = points4D[0:3,:]/points4D[3,:]
+print("Points:")
+print(points3D)
+print("Dist:")
+print(np.sqrt(points3D[0]*points3D[0] + points3D[1]*points3D[1] + points3D[2]*points3D[2]))
+
+# f = open("cones.csv", "a")
+# for x, y, z in points3D.transpose():
+#     f.write(str(x) + "," + str(y) + "," + str(z) + "\n")
+
+# f.close()
+
+cv.waitKey(0)
