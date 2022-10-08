c9d6a4911e1d551eac63b23e9bfe4a14876ae772
Bill Clayton <bill_clayton@outlook.com>
2022-10-08 17:48:42 +1100

    Created the cone detection node. Also removed unused files from mur2022

--- catkin_ws/src/mur2022/src/topic_names.h
+++ catkin_ws/src/mur2022/src/topic_names.h
@@ -9,7 +9,7 @@
 #define POINT_CLOUD_SECTION_TOPIC "/point_cloud_requested"
 #define POINT_CLOUD_SECTION_REQUEST_TOPIC "/send_point_cloud"
 #define CONE_DETECTED_TOPIC "/stereo_cones"
-#define CONES_FULL_TOPIC "/full_cones"
+#define CONES_FULL_TOPIC "/mur/slam/cones"
 #define CONTROL_MAP_TOPIC "/mur/planner/map"
 #define CONTROL_PATH_TOPIC "/mur/planner/path"
 #define CONTROL_TRANSITION_TOPIC "/mur/control/transition"
