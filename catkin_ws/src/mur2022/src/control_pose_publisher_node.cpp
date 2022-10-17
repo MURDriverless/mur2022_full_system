ed2abedf7c8128b6a31f3d949d77ec11a225cef6
Micah-Swenson <m.swenson@optusnet.com.au>
2022-10-18 09:05:57 +1100

    Fixed control pose transform direction

--- catkin_ws/src/mur2022/src/control_pose_publisher_node.cpp
+++ catkin_ws/src/mur2022/src/control_pose_publisher_node.cpp
@@ -44,11 +44,11 @@ int main(int argc, char** argv){
       
       tf::StampedTransform transform;
       listener.waitForTransform(HUSKY_FRAME, GLOBAL_FRAME, ros::Time::now(), ros::Duration(1.0));
-      listener.lookupTransform(HUSKY_FRAME, GLOBAL_FRAME, this_time, transform);
+      listener.lookupTransform(GLOBAL_FRAME, HUSKY_FRAME, this_time, transform);
 
       geometry_msgs::Twist velocity;
       listener.waitForTransform(HUSKY_FRAME, GLOBAL_FRAME, ros::Time::now(), ros::Duration(1.0));
-      listener.lookupTwist(HUSKY_FRAME, GLOBAL_FRAME, this_time,ros::Duration(0.3), velocity);
+      listener.lookupTwist(GLOBAL_FRAME, HUSKY_FRAME, this_time,ros::Duration(0.3), velocity);
 
       tf::Vector3 position = transform.getOrigin();
       tf::Quaternion orientation = transform.getRotation();
